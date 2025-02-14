bd26ff60dc7598d7bdd204dabd3c82ed41d32487
Raymond Nambaale <nambaaler@gmail.com>
2025-02-14 15:54:09 +0300

    Melting quotes

--- src/primitives.rs
+++ src/primitives.rs
@@ -100,3 +100,82 @@ pub struct PostMintBtcOnchainRequest {
 pub struct PostMintBtcOnchainResponse {
     pub signatures: Vec<BlindedSignature>,
 }
+
+#[derive(Deserialize, Serialize, Debug, Clone, ToSchema)]
+pub struct PostMeltQuoteBtcOnchainRequest {
+    pub amount: u64,
+    /// onchain address
+    pub address: String,
+    // pub unit: CurrencyUnit,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone, ToSchema)]
+pub struct PostMeltQuoteBtcOnchainResponse {
+    pub quote: String,
+    pub description: Option<String>,
+    pub amount: u64,
+    pub fee: u64,
+    pub state: MeltBtcOnchainState,
+    pub expiry: u64,
+}
+
+impl From<BtcOnchainMeltQuote> for PostMeltQuoteBtcOnchainResponse {
+    fn from(quote: BtcOnchainMeltQuote) -> Self {
+        Self {
+            quote: quote.quote_id.to_string(),
+            amount: quote.amount,
+            fee: quote.fee_total,
+            expiry: quote.expiry,
+            state: quote.state,
+            description: quote.description,
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq, Clone, ToSchema)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum MeltBtcOnchainState {
+    /// initial state. No payment received from the wallet yet
+    Unpaid,
+
+    /// the mint received the payment from the wallet, but did not broadcast the transaction yet
+    Pending,
+
+    /// the mint broadcasted the btc onchain transaction
+    Paid,
+}
+
+impl Display for MeltBtcOnchainState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MeltBtcOnchainState::Unpaid => write!(f, "UNPAID"),
+            MeltBtcOnchainState::Pending => write!(f, "PENDING"),
+            MeltBtcOnchainState::Paid => write!(f, "PAID"),
+        }
+    }
+}
+
+impl FromStr for MeltBtcOnchainState {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "UNPAID" => Ok(MeltBtcOnchainState::Unpaid),
+            "PENDING" => Ok(MeltBtcOnchainState::Pending),
+            "PAID" => Ok(MeltBtcOnchainState::Paid),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct BtcOnchainMeltQuote {
+    pub quote_id: Uuid,
+    pub amount: u64,
+    pub address: String,
+    pub fee_total: u64,
+    pub fee_sat_per_vbyte: u32,
+    pub expiry: u64,
+    pub state: MeltBtcOnchainState,
+    pub description: Option<String>,
+}
