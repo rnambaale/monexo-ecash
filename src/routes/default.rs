8b33113a8f04a85c9fb951f45c668f444d168508
Raymond Nambaale <nambaaler@gmail.com>
2025-02-20 19:52:52 +0300

    Load mint usdc ata from wallet key pair

--- src/routes/default.rs
+++ src/routes/default.rs
@@ -1,5 +1,8 @@
+use std::str::FromStr;
+
 use axum::{extract::{Path, State}, Json};
 use monexo_core::{keyset::Keysets, primitives::{CurrencyUnit, KeyResponse, KeysResponse, MintInfoResponse, PostSwapRequest, PostSwapResponse}};
+use solana_sdk::{pubkey::Pubkey, signature::Keypair, signer::{EncodableKey, Signer}};
 
 use crate::{error::MonexoMintError, mint::Mint};
 
@@ -34,17 +37,27 @@ pub async fn post_swap(
         (status = 200, description = "get mint info", body = [MintInfoResponse])
     )
 )]
-#[instrument(name = "get_info", skip(_mint), err)]
-pub async fn get_info(State(_mint): State<Mint>) -> Result<Json<MintInfoResponse>, MonexoMintError> {
+#[instrument(name = "get_info", skip(mint), err)]
+pub async fn get_info(State(mint): State<Mint>) -> Result<Json<MintInfoResponse>, MonexoMintError> {
     // let mint_info = mint.config.info.clone();
+
+    let usdc_mint_address = Pubkey::from_str("4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU").unwrap();
+    let monexo_wallet_keypair = Keypair::read_from_file(
+        mint.config.derivation_path.unwrap()
+    ).expect("Failed to load keypair");
+    let monexo_usdc_ata = spl_associated_token_account::get_associated_token_address(
+        &monexo_wallet_keypair.try_pubkey().expect("Failed to load mint pubkey"),
+        &usdc_mint_address,
+    );
 
     let mint_info = MintInfoResponse {
         // name: mint.config.info.name,
         name: None,
         version: None,
-        usdc_address: String::from("HVasUUKPrmrAuBpDFiu8BxQKzrMYY5DvyuNXamvaG2nM"),
-        usdc_token_mint: String::from("4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU&reference=5t6gQ7Mnr3mmsFYquFGwgEKokq9wrrUgCpwWab93LmLL")
+        usdc_address: monexo_usdc_ata.to_string(),
+        usdc_token_mint: usdc_mint_address.to_string()
     };
+
     Ok(Json(mint_info))
 }
 
@@ -81,3 +94,50 @@ pub async fn get_keys_by_id(
         }],
     }))
 }
+
+// Usage
+// use solana_client::nonblocking::rpc_client::RpcClient;
+// let rpc_url = "https://api.devnet.solana.com";
+// let client = RpcClient::new(rpc_url.to_string());
+// let source_ata = create_ata(
+//     &client,
+//     &monexo_wallet_keypair,
+//     &monexo_wallet_keypair.try_pubkey().expect("Failed to load mint pubkey"),
+//     &usdc_mint_address
+// ).await.expect("failed to create ata");
+
+// let sender_balance = client.get_token_account_balance(&source_ata).await?;
+// info!("Sender's USDC Balance: {:?}", sender_balance);
+// info!(
+//     "source_ata: {:?}", source_ata
+// );
+
+// async fn create_ata(
+//     rpc_client: &RpcClient,
+//     payer: &Keypair, // Your server wallet
+//     owner: &Pubkey,
+//     mint: &Pubkey
+// ) -> Result<Pubkey, Box<dyn std::error::Error>> {
+
+//     let ata = spl_associated_token_account::get_associated_token_address(owner, mint);
+
+//     let ix = spl_associated_token_account::instruction::create_associated_token_account(
+//         &payer.pubkey(),
+//         owner,
+//         mint,
+//         &spl_token::id()
+//     );
+//     let recent_blockhash = rpc_client.get_latest_blockhash().await?;
+
+//     let tx = solana_sdk::transaction::Transaction::new_signed_with_payer(
+//         &[ix],
+//         Some(&payer.pubkey()),
+//         &[payer],
+//         recent_blockhash,
+//     );
+
+//     let sig = rpc_client.send_and_confirm_transaction(&tx).await?;
+//     println!("ATA created: {:?}", sig);
+
+//     Ok(ata)
+// }
